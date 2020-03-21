8360b8616b4d002f7e2005dd2cbd025fed8c4276
rex.chen <rex.chen@appier.com>
2020-03-21 23:52:35 +0800

    0.2.0

---
+++ HeyAiquaSdk/Classes/HeySdk.h
@@ -0,0 +1,16 @@
+//
+//  HeySdk.h
+//  HeyAiquaSdk
+//
+//  Created by chunta on 2020/3/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HeySdk : NSObject
+- (void)hey;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HeyAiquaSdk/Classes/HeySdk.m
@@ -0,0 +1,14 @@
+//
+//  HeySdk.m
+//  HeyAiquaSdk
+//
+//  Created by chunta on 2020/3/21.
+//
+
+#import "HeySdk.h"
+
+@implementation HeySdk
+- (void)hey {
+    NSLog(@"Hey SDK");
+}
+@end
